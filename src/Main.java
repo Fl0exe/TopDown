3a8030902a774c4b6d574157ed61ec24eff8d57e
Fl0exe <florian.pabler@icloud.com>
2022-10-12 21:10:07 +0200

    *Done Coding*

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import bin.Kontrolle;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Kontrolle kontrolle = new Kontrolle();
+		kontrolle.starteSpiel();
+
+	}
+
+}
---
+++ src/bin/Kontrolle.java
@@ -0,0 +1,11 @@
+package bin;
+
+public class Kontrolle {
+	
+	public void starteSpiel() {
+		
+		System.out.println("BAN");
+		
+	}
+
+}
